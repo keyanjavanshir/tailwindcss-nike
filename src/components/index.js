96df27144f1260874582f817f51bd9ea1feb243e
Keyan <keyanjavanshir@live.dk>
2024-02-02 13:13:00 +0100

    Finished CustomerReview section. Added ReviewCard component

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@
 import PopularProductCard from './PopularProductCard';
 import Button from './Button';
 import ServiceCard from './ServiceCard';
+import ReviewCard from './ReviewCard';
 
 export {
     PopularProductCard,
     Button,
-    ServiceCard
+    ServiceCard,
+    ReviewCard
 }
